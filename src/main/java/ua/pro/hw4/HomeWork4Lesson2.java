2e13f81701761c3fcd4d9e4b49baf74b5a97ba71
Digriz87 <fiveadvert@gmail.com>
2021-09-05 23:38:16 +0300

    home work 4 lesson 2 start

--- src/main/java/ua/pro/hw4/HomeWork4Lesson2.java
+++ src/main/java/ua/pro/hw4/HomeWork4Lesson2.java
@@ -4,6 +4,26 @@ public class HomeWork4Lesson2
 {
     public static void main(String[] args)
     {
+        int yearInput = (100);
+        int finalCentury = centuryFromYear(yearInput);
+        if (yearInput <= 100)
+        {
+            System.out.println("Это " + finalCentury + " век до нашей эры" );
+        } else
+            {
+                System.out.println("Это " + finalCentury + " век нашей эры");
+            }
 
+    }
+
+    static int centuryFromYear(int yearInput)
+    {
+        if (yearInput % 100 == 0)
+        {
+            yearInput = yearInput / 100;
+        } else {
+            yearInput = (yearInput / 100) + 1;
+        }
+        return yearInput;
     }
 }
