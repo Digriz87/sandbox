0450264be22c36697ed072b849d69f71b7de4308
Digriz87 <fiveadvert@gmail.com>
2021-10-31 17:12:08 +0300

    home work lesson IO Done

--- src/main/java/ua/pro/hw20/Main.java
+++ src/main/java/ua/pro/hw20/Main.java
@@ -1,57 +1,100 @@
 package ua.pro.hw20;
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
+        LinkedHashMap<String, Integer> sortedMapWords = new LinkedHashMap<>();
+        Map<String,Integer> nonSortedWords=new HashMap<>();
 
-        String Default = "THE TRAGEDY OF HAMLET PRINCE OF DENMARK.txt";
-        InputStream inputStream;
-        PrintWriter outputStream;
+        String pathInput= "THE TRAGEDY OF HAMLET PRINCE OF DENMARK.txt";
+        String pathOutput="Calc.txt";
 
-        Map<String,Integer> words=new HashMap<>();
+        BufferedReader inputStream = null;
+        PrintWriter outputStream = null;
 
-        CountWords(Default,words);
-        System.out.println(words);
+        try {
+            inputStream = new BufferedReader(new FileReader(pathInput));
+            outputStream = new PrintWriter(new FileWriter(pathOutput));
+            outputStream.println("Analyzed file: " + pathOutput);
+            outputStream.println("word | frequency");
+            CountWords(inputStream,nonSortedWords);
+            Sorting(nonSortedWords,sortedMapWords);
 
-        Set<Map.Entry<String, Integer> > entrySet
-                = words.entrySet();
-        ArrayList<Map.Entry<String, Integer> > listOfEntry
-                = new ArrayList<>(entrySet);
-
-
-
-        inputStream = new FileInputStream(Default);
-        outputStream = new PrintWriter(new FileOutputStream("Calculation.txt"));
-        outputStream.println("Analyzed file: " + Default);
-        outputStream.println("word | frequency");
-        for (Map.Entry<String, Integer> line: listOfEntry){
-            outputStream.println(String.format("%10s | %10d", line.getKey(),line.getValue()));
-
+            for (Map.Entry<String, Integer> line: sortedMapWords.entrySet()){
+                outputStream.println(String.format("%10s | %10d", line.getKey(),line.getValue()));
+            }
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (outputStream != null) {
+                outputStream.close();
+            }
         }
-
-
-
-
-
     }
 
-    private static void CountWords(String s, Map<String, Integer> words) throws FileNotFoundException {
-        Scanner file=new Scanner (new File(s));
+    private static void Sorting(Map<String, Integer> nonSortedWords, LinkedHashMap<String, Integer> sortedMapWords) {
+        ArrayList<Integer> list = new ArrayList<>();
 
+        for (Map.Entry<String, Integer> entry : nonSortedWords.entrySet()) {
+            list.add(entry.getValue());
+        }
+        Collections.sort(list, Collections.reverseOrder());
+        for (int num : list) {
+            for (Map.Entry<String, Integer> entry : nonSortedWords.entrySet()) {
+                if (entry.getValue().equals(num)) {
+                    sortedMapWords.put(entry.getKey(), num);
+                }
+            }
+
+        }
+    }
+
+    private static void CountWords(BufferedReader inputStream, Map<String, Integer> nonSortedWords) throws FileNotFoundException {
+        Scanner file=new Scanner (inputStream);
+        file.next().replace(" ","");
+        String whitespace_chars =  ""       /* dummy empty string for homogeneity */
+                + "\\u0009" // CHARACTER TABULATION
+                + "\\u000A" // LINE FEED (LF)
+                + "\\u000B" // LINE TABULATION
+                + "\\u000C" // FORM FEED (FF)
+                + "\\u000D" // CARRIAGE RETURN (CR)
+                + "\\u0020" // SPACE
+                + "\\u0085" // NEXT LINE (NEL)
+                + "\\u00A0" // NO-BREAK SPACE
+                + "\\u1680" // OGHAM SPACE MARK
+                + "\\u180E" // MONGOLIAN VOWEL SEPARATOR
+                + "\\u2000" // EN QUAD
+                + "\\u2001" // EM QUAD
+                + "\\u2002" // EN SPACE
+                + "\\u2003" // EM SPACE
+                + "\\u2004" // THREE-PER-EM SPACE
+                + "\\u2005" // FOUR-PER-EM SPACE
+                + "\\u2006" // SIX-PER-EM SPACE
+                + "\\u2007" // FIGURE SPACE
+                + "\\u2008" // PUNCTUATION SPACE
+                + "\\u2009" // THIN SPACE
+                + "\\u200A" // HAIR SPACE
+                + "\\u2028" // LINE SEPARATOR
+                + "\\u2029" // PARAGRAPH SEPARATOR
+                + "\\u202F" // NARROW NO-BREAK SPACE
+                + "\\u205F" // MEDIUM MATHEMATICAL SPACE
+                + "\\u3000" // IDEOGRAPHIC SPACE
+                ;
+
+                String pattern = "[^\\d\\w]+";
         while(file.hasNext()){
-            String word=file.next().replaceAll("[^\\d\\w]+","").toLowerCase(Locale.ROOT);
-            Integer count=words.get(word);
+            file.next().replaceAll(whitespace_chars,"");
+            String word=file.next().replaceAll(pattern,"").toLowerCase(Locale.ROOT);
+            Integer count=nonSortedWords.get(word);
             if(count!=null)
                 count++;
             else
                 count=1;
-            words.put(word,count);
+            nonSortedWords.put(word,count);
         }
         file.close();
     }
 
 }
-
-
